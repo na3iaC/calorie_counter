8e64d638c11350993a618b60f377da82933f28b2
na3iaC <nazia.chowdhury99@gmail.com>
2024-03-04 11:16:20 +0000

    new backend

--- backend_server/app/routes/calorie.routes.js
+++ backend_server/app/routes/calorie.routes.js
@@ -2,5 +2,8 @@ const cal = require('../controllers/calorie.controllers');
 
 module.exports = function(app){
     app.route('/food/search') // Note the route change from '/foods/search' to '/food/search'
-   .get(cal.getSingleFood);
+   .get(cal.searching);
+
+   app.route('/food/:foodId')
+   .get(cal.getFoodItemDetails);
 }
