4246bfc389940d9b064ce737da3c29e286641cce
na3iaC <nazia.chowdhury99@gmail.com>
2024-03-08 17:46:33 +0000

    single item change

---
+++ frontend/vue-project/src/views/pages/Single_item.vue
@@ -0,0 +1,32 @@
+<script>
+import { foodService } from "../../services/food.service";
+ 
+export default {
+  data() {
+    return {
+      post: {},
+      error: "",
+    };
+  },
+  created() {
+    this.post.loading = true;
+ 
+    postService
+      .getSinglePost(this.$route.params.id)
+      .then((post) => {
+        this.post = post;
+      })
+      .catch((error) => (this.error = error));
+  },
+  methods: {
+    handleSubmit(e) {
+      this.submitted = true;
+      const { new_post } = this;
+ 
+      if (!new_post) {
+        return;
+      }
+    },
+  },
+};
+</script>
